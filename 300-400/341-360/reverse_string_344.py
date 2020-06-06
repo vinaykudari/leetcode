387351d0cf0bc059352cabe081a138b75f41286f
Vinay Kudari <svvk.20140513@btech.nitdgp.ac.in>
2020-06-06 23:42:05 +0530

    June Challenge, Day 5

---
+++ 300-400/341-360/reverse_string_344.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        def reverse(s, l, i=0):
+            if i == l//2:
+                return
+            reverse(s, l, i+1)
+            s[i], s[l-i-1] = s[l-i-1], s[i]
+            
+        reverse(s, len(s))
+        
---
+++ 500-600/521-540/random_pick_with_weight_528.py
@@ -0,0 +1,18 @@
+import random
+import bisect
+
+class Solution:
+
+    def __init__(self, w: List[int]):
+        for i in range(1, len(w)):
+            w[i] += w[i-1] 
+        self.w = w
+        
+    def pickIndex(self) -> int:
+        return bisect.bisect_left(self.w, random.randint(1, self.w[-1]))
+                 
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
