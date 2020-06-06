9dc9d90033976a0ae4b1be2b14c28bf0af90e2ba
Vinay Kudari <svvk.20140513@btech.nitdgp.ac.in>
2020-06-07 01:33:49 +0530

    June Challenge, Day 6

---
+++ 400-500/400-420/queue_reconstruction_by_height_406.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reconstructQueue(self, people):
+        people.sort(key=lambda x: (-x[0], x[1]))
+        res = []
+        for p in people:
+            res.insert(p[1], p)
+        return res
+
+            
+            
+        
