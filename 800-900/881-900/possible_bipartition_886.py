bccfb2cbc5a3ee428b02958fc07ef8860a2955b2
Vinay Kudari <svvk.20140513@btech.nitdgp.ac.in>
2020-06-16 01:22:18 +0530

    graph-bfs-colouring

--- 800-900/881-900/possible_bipartition_886.py
+++ 800-900/881-900/possible_bipartition_886.py
@@ -43,7 +43,46 @@ class Solution:
             return True
                 
                 
+# Graph - BFS - Two Color
+
+from collections import deque
+class Solution:
+    def possibleBipartition(self, N: int, dislikes: List[List[int]]) -> bool:
+        if not dislikes:
+            return True
+        
+        def get_adjacency_list(dislikes, N):
+            adjacency_list = defaultdict(list)
+            for dislike in dislikes:
+                adjacency_list[dislike[0]-1].append(dislike[1]-1)
+                adjacency_list[dislike[1]-1].append(dislike[0]-1)
+            return adjacency_list
+        
+        def bfs_is_bipartite(adjacency_list, N):
+            visited = [False] * N
+            color = [-1] * N
             
+            for i in range(N):
+                queue = deque([i])
+                visited[i] = True
+
+                while queue:
+                    u = queue.pop()
+                    if color[u] == -1:
+                        color[u] = True
+                    for v in adjacency_list[u]:
+                        if color[u] == color[v]:
+                            return False
+                        if not visited[v]:
+                            queue.append(v)
+                            visited[v] = True
+                            color[v] = not color[u]
+
+            return True
+            
+        
+        adjacency_list = get_adjacency_list(dislikes, N)
+        return bfs_is_bipartite(adjacency_list, N)            
                 
         
         
