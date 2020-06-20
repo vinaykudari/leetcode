8bccdb3489e338b76be9ff230a265406363b979e
Vinay Kudari <svvk.20140513@btech.nitdgp.ac.in>
2020-06-21 04:40:14 +0530

    tree-iteration

--- 200-300/221-240/kth_smallest_element_in_a_BST_230.py
+++ 200-300/221-240/kth_smallest_element_in_a_BST_230.py
@@ -46,4 +46,27 @@ class Solution:
         helper(root)
         return stack[k-1]
         
+----------------------------------Tree-Iteration--------------------------------
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        stack = []
         
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -= 1
+            if k == 0:
+                return root.val
+            root = root.right
+
+        
+                
