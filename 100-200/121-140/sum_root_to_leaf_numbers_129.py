b2229490d8bcb3576488c68a7bb5eb2419dcaef2
Vinay Kudari <svvk.20140513@btech.nitdgp.ac.in>
2020-06-26 23:03:06 +0530

    tree-recursion

---
+++ 100-200/121-140/sum_root_to_leaf_numbers_129.py
@@ -0,0 +1,64 @@
+Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+
+An example is the root-to-leaf path 1->2->3 which represents the number 123.
+
+Find the total sum of all root-to-leaf numbers.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Input: [1,2,3]
+    1
+   / \
+  2   3
+Output: 25
+Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25.
+Example 2:
+
+Input: [4,9,0,5,1]
+    4
+   / \
+  9   0
+ / \
+5   1
+Output: 1026
+Explanation:
+The root-to-leaf path 4->9->5 represents the number 495.
+The root-to-leaf path 4->9->1 represents the number 491.
+The root-to-leaf path 4->0 represents the number 40.
+Therefore, sum = 495 + 491 + 40 = 1026.
+
+---------------------Tree-Recusion----------------------
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: TreeNode) -> int:
+        total = 0
+        def helper(root, num):
+            nonlocal total
+            if root is None:
+                return None
+            
+            num = num*10 + root.val 
+            left = helper(root.left, num)
+            right = helper(root.right, num)
+            
+            if (left is None) and (right is None):
+                total += num
+                num = num//10
+                
+            return num
+                
+        helper(root, 0)
+        return total
+        
