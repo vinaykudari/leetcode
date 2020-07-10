ca4355fea267371e65a74a401809603b7bc0db1b
Vinay Kudari <svvk.20140513@btech.nitdgp.ac.in>
2020-07-11 01:54:15 +0530

    linked-list

---
+++ 400-500/421-440/flatten_a_multi_level_doubly_linked_list_430.py
@@ -0,0 +1,49 @@
+You are given a doubly linked list which in addition to the next and previous pointers, it could have a child pointer, which may or may not point to a separate doubly linked list. These child lists may have one or more children of their own, and so on, to produce a multilevel data structure, as shown in the example below.
+
+Flatten the list so that all the nodes appear in a single-level, doubly linked list. You are given the head of the first level of the list.
+
+ 
+
+Example 1:
+
+Input: head = [1,2,3,4,5,6,null,null,null,7,8,9,10,null,null,11,12]
+Output: [1,2,3,7,8,11,12,9,10,4,5,6]
+Explanation:
+
+The multilevel linked list in the input is as follows:
+
+
+
+After flattening the multilevel linked list it becomes:
+
+---------------------Linked-List----------------------
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+
+class Solution:
+    def flatten(self, head: 'Node') -> 'Node':
+        stack = []
+        node = head
+        
+        while node:
+            if node.child:
+                if node.next:
+                    stack.append(node.next)
+                node.next = node.child
+                node.child.prev = node
+                node.child = None    
+            if stack and node.next is None:
+                node.next = stack.pop()
+                node.next.prev = node
+                
+            node = node.next
+            
+        return head
