6f8972d3dbc6952af81f05a515787e9dc3ad24bd
Vinay Kudari <svvk.20140513@btech.nitdgp.ac.in>
2020-07-12 17:37:09 +0530

    math-bit-manipulation

---
+++ 100-200/181-200/reverse_bits_190.py
@@ -0,0 +1,27 @@
+Reverse bits of a given 32 bits unsigned integer.
+
+ 
+
+Example 1:
+
+Input: 00000010100101000001111010011100
+Output: 00111001011110000010100101000000
+Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 which its binary representation is 00111001011110000010100101000000.
+Example 2:
+
+Input: 11111111111111111111111111111101
+Output: 10111111111111111111111111111111
+Explanation: The input binary string 11111111111111111111111111111101 represents the unsigned integer 4294967293, so return 3221225471 which its binary representation is 10111111111111111111111111111111.
+
+
+---------------Bit-Manipulation-----------------
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+        for power in range(31, -1, -1):
+            last_bit = n & 1
+            res += last_bit << power
+            n >>= 1
+            
+        return res
