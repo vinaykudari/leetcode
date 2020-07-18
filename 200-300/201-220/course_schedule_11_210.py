a736f402e6dcf8413414e79dd4b00745b7a15e5e
Vinay Kudari <svvk.20140513@btech.nitdgp.ac.in>
2020-07-19 03:30:51 +0530

    dfs-topological-sort

---
+++ 200-300/201-220/course_schedule_11_210.py
@@ -0,0 +1,60 @@
+There are a total of n courses you have to take, labeled from 0 to n-1.
+
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+Given the total number of courses and a list of prerequisite pairs, return the ordering of courses you should take to finish all courses.
+
+There may be multiple correct orders, you just need to return one of them. If it is impossible to finish all courses, return an empty array.
+
+Example 1:
+
+Input: 2, [[1,0]] 
+Output: [0,1]
+Explanation: There are a total of 2 courses to take. To take course 1 you should have finished   
+             course 0. So the correct course order is [0,1] .
+Example 2:
+
+Input: 4, [[1,0],[2,0],[3,1],[3,2]]
+Output: [0,1,2,3] or [0,2,1,3]
+Explanation: There are a total of 4 courses to take. To take course 3 you should have finished both     
+             courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0. 
+             So one correct course order is [0,1,2,3]. Another correct ordering is [0,2,1,3] .
+Note:
+
+The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+You may assume that there are no duplicate edges in the input prerequisites.
+
+---------------------------------dfs-topological-sort----------------------------------
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        def get_adj():
+            adj = [[] for _ in range(numCourses)]
+            for course, req in prerequisites:
+                adj[req].append(course)
+                
+            return adj
+        
+        def dfs(u, visited, curr_visited):
+            visited[u] = True
+            curr_visited[u] = True
+            for v in adj[u]:
+                if curr_visited[v]:
+                    return True
+                if not visited[v]:
+                    if dfs(v, visited, curr_visited):
+                        return True
+            curr_visited[u] = False
+            order.append(u)
+            
+            return False
+        
+        adj = get_adj()
+        order = []
+        visited = [False]*numCourses
+        for u in range(numCourses):
+            if not visited[u]:
+                if dfs(u, visited, [False]*numCourses):
+                    return []
+                
+        return order[::-1]
