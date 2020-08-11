b52ce312c5e2448bf02f3455741602d2dfc71f1f
Vinay Kudari <svvk.20140513@btech.nitdgp.ac.in>
2020-08-12 01:12:42 +0530

    easy

---
+++ 100-200/161-180/excel_sheet_column_number_171.py
@@ -0,0 +1,42 @@
+Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+For example:
+
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28 
+    ...
+Example 1:
+
+Input: "A"
+Output: 1
+Example 2:
+
+Input: "AB"
+Output: 28
+Example 3:
+
+Input: "ZY"
+Output: 701
+ 
+
+Constraints:
+
+1 <= s.length <= 7
+s consists only of uppercase English letters.
+s is between "A" and "FXSHRXW".
+
+-----------------O(n)-------------------
+
+class Solution:
+    def titleToNumber(self, s: str) -> int:
+        res, i = 0, 0
+        for ch in s[::-1]:
+            res += (ord(ch) - ord('A') + 1) * 26**i
+            i += 1
+            
+        return res
